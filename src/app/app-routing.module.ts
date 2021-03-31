3c4adbc13b7c93a2999f90eee2a257c8cf4209a9
sandySandiaGit <mailtosandia@yahoo.com>
2021-03-31 14:13:01 +0200

    exo2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
+ 
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
-  {
+  /*{
     path: 'first-page',
     loadChildren: () => import('./first-page/first-page.module').then( m => m.FirstPagePageModule)
-  },
+  },*/
+  {
+    path: 'first-page/:id',
+    loadChildren: () => import('./first-page/first-page.module').then( m => m.FirstPagePageModule)
+  }
+  
 ];
 
 @NgModule({
--- src/app/first-page/first-page.page.ts
+++ src/app/first-page/first-page.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-first-page',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstPagePage implements OnInit {
 
-  constructor() { }
-
+  constructor(private activatedRoute: ActivatedRoute) { }
+  public name: string;
   ngOnInit() {
+    this.name = this.activatedRoute.snapshot.paramMap.get('id');
   }
 
 }
